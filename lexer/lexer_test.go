7aa0863be21cda071d58d7ed96503547c1843bca
shozawa <tomohiro.shozawa@gmail.com>
2019-05-04 16:05:10 +0900

    Lexerのテストを追加

---
+++ lexer/lexer_test.go
@@ -0,0 +1,38 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/shozawa/monkey2/token"
+)
+
+func TestLexer(t *testing.T) {
+	tests := []struct {
+		input string
+		want  []token.Token
+	}{
+		{"5;", []token.Token{
+			token.Token{Type: token.INT, Literal: "5"},
+			token.Token{Type: token.SEMICOLON, Literal: ";"},
+			token.Token{Type: token.EOF, Literal: ""},
+		}},
+		{"42;", []token.Token{
+			token.Token{Type: token.INT, Literal: "42"},
+			token.Token{Type: token.SEMICOLON, Literal: ";"},
+			token.Token{Type: token.EOF, Literal: ""},
+		}},
+	}
+
+	for _, test := range tests {
+		l := New(test.input)
+		for _, tok := range test.want {
+			got := l.NextToken()
+			if got.Type != tok.Type {
+				t.Errorf("token.Type is not %q. got=%q", tok.Type, got.Type)
+			}
+			if got.Literal != tok.Literal {
+				t.Errorf("token.Literal is not %q. got=%q", tok.Literal, got.Literal)
+			}
+		}
+	}
+}
