13716cf11a4a84266a298b46995b3cdfed2d5879
shozawa <tomohiro.shozawa@gmail.com>
2019-05-04 18:14:44 +0900

    Parserがast.Programを吐き出すところまでを実装

--- parser/parser.go
+++ parser/parser.go
@@ -2,23 +2,38 @@ package parser
 
 import (
 	"github.com/shozawa/monkey2/ast"
-	// "github.com/shozawa/monkey2/token"
 	"github.com/shozawa/monkey2/lexer"
+	"github.com/shozawa/monkey2/token"
 )
 
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l}
+	p.nextToken()
 	return p
 }
 
 type Parser struct {
-	l *lexer.Lexer
+	l        *lexer.Lexer
+	curToken token.Token
 }
 
 func (p *Parser) Parse() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseExpressionStatement()
+		program.Statements = append(program.Statements, stmt)
+		p.nextToken()
+	}
+	return program
 }
 
-func (p *Parser) nextToken() ast.Node {
-	return nil
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	for p.curToken.Type != token.SEMICOLON {
+		p.nextToken()
+	}
+	return &ast.ExpressionStatement{}
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.l.NextToken()
 }
