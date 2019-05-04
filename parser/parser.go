a5a42790ff930f645df9a28f5771051f3c8141e0
shozawa <tomohiro.shozawa@gmail.com>
2019-05-04 20:31:55 +0900

    二項演算子に対応（優先順位は考慮せず）

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/shozawa/monkey2/ast"
 	"github.com/shozawa/monkey2/lexer"
 	"github.com/shozawa/monkey2/token"
@@ -30,22 +32,55 @@ func (p *Parser) Parse() *ast.Program {
 }
 
 func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
-	var exp ast.Expression
-	switch p.curToken.Type {
-	case token.INT:
-		exp = p.parseInteger()
-	}
+	exp := p.parseExpression(0)
 	for p.curToken.Type != token.SEMICOLON {
 		p.nextToken()
 	}
 	return &ast.ExpressionStatement{Expression: exp}
 }
 
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	var left ast.Expression
+	switch p.curToken.Type {
+	case token.INT:
+		left = p.parseInteger()
+	default:
+		msg := fmt.Sprintf("unexpected token %q", p.curToken.Type)
+		panic(msg)
+	}
+
+	for !p.peekTokenIs(token.SEMICOLON) && p.peekPrecedence() > precedence {
+		p.nextToken()
+		left = p.parseInfixExpression(left)
+	}
+
+	return left
+}
+
+func (p *Parser) peekPrecedence() int {
+	return 1
+}
+
 func (p *Parser) parseInteger() *ast.Integer {
 	return &ast.Integer{Token: p.curToken}
+}
+
+func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
+	infix := &ast.InfixExpression{
+		Operator: p.curToken.Literal,
+		Left:     left,
+	}
+	p.nextToken() // consume operator
+	// FIXME: 演算子の優先順位を考慮していない
+	infix.Right = p.parseExpression(0)
+	return infix
 }
 
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) peekTokenIs(tokeType token.TokenType) bool {
+	return p.peekToken.Type == tokeType
 }
